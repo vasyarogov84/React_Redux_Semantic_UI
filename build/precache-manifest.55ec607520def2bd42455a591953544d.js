846ba282eb0245d72bf20f10e1bff6d73830f235
vasyarogov84 <viktorgrom84@gmail.com>
2018-12-31 14:43:18 -0800

    delete yarn-error for test

---
+++ build/precache-manifest.55ec607520def2bd42455a591953544d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dc1fddc13707d8a1932",
+    "url": "/static/css/main.210bdd9e.chunk.css"
+  },
+  {
+    "revision": "3dc1fddc13707d8a1932",
+    "url": "/static/js/main.3dc1fddc.chunk.js"
+  },
+  {
+    "revision": "58e58cb98d7749411a54",
+    "url": "/static/js/1.58e58cb9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5c81e8aef9f8156aecf068b216e5fc3",
+    "url": "/index.html"
+  }
+];
